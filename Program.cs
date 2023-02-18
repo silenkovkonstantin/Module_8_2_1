0db8d4a4bc281668246cf3e17fa3162843f55c65
silenkovkonstantin <silenkov@inbox.ru>
2023-02-18 08:29:22 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Module_8_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetDiskObjectsCount(); //   Вызов метода получения директорий
+        }
+
+        static void GetDiskObjectsCount()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives(); // Получаем массив дисков
+
+            foreach (var item in drives)
+            {
+                DirectoryInfo[] dirs = item.RootDirectory.GetDirectories(); // Получаем массив каталогов на диске
+                FileInfo[] files = item.RootDirectory.GetFiles(); // Получаем массив файлов на диске
+                Console.WriteLine($"Общее количество каталогов и файлов на диске {item}: {dirs.Length + files.Length}");
+            }
+        }
+    }
+}
